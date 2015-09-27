f436c8518541d94f5a597b7ea6028b8ca887de12
hashmee <hashmee.smf@gmail.com>
2015-09-27 20:03:01 +0530

    basic crud operations for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,43 @@
 class PostsController < ApplicationController
+    before_action :get_post, only: [:show, :edit, :update, :destroy]
 	def index
-		
+      @posts = Post.all		
 	end
 
 	def new
-
+      @post = Post.new
 	end
 
 	def create	
-
+      @post = Post.new(post_params)
+      @post.save
+      redirect_to posts_path, notice: 'Post is successfully created'
     end
 
     def show
-    	
     end
 
     def edit
-
     end
 
     def update
-
+     @post.update(post_params)
+     redirect_to posts_path
     end
 
     def destroy
-    	
+      @post.destroy
+      redirect_to posts_path
     end
+
+ private
+   
+   def post_params
+     params.require(:post).permit(:name, :description)
+   end
+
+   def get_post
+    @post = Post.find(params[:id])
+   end
 
 end
